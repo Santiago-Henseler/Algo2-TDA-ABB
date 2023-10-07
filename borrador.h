edf180680e86d8a124e61dc65ad44d394db3d28b
Santiago-Henseler <116313638+Santiago-Henseler@users.noreply.github.com>
2023-10-07 17:38:12 -0300

    Add files via upload

---
+++ borrador.h
@@ -0,0 +1,10 @@
+#ifndef _BORRADOR_H_
+#define _BORRADOR_H_
+
+#include "funcionalidades.h"
+
+void destruir_todo_iterativa(struct nodo_abb* actual, void (*destructor)(void *));
+
+struct nodo_abb *quitar_recursivo(abb_t *arbol, void *elemento, struct nodo_abb* actual, almacenador_t * encontrado);
+
+#endif // _BORRADOR_H_
